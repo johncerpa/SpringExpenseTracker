073da4c7f6634be83334ca6d35ed242382e2ac4b
John Cerpa <jjcerpa@uninorte.edu.co>
2020-08-06 16:39:04 -0500

    Added CORS

--- src/main/java/com/myproject/expensetrackerapi/ExpenseTrackerApiApplication.java
+++ src/main/java/com/myproject/expensetrackerapi/ExpenseTrackerApiApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class ExpenseTrackerApiApplication {
@@ -21,6 +24,27 @@ public class ExpenseTrackerApiApplication {
 		// endpoint is protected (user needs to provide JWT to verify login)
 		registrationBean.setFilter(authFilter);
 		registrationBean.addUrlPatterns("/api/categories/*");
+
+		return registrationBean;
+	}
+
+	@Bean
+	public FilterRegistrationBean<CorsFilter> myCorsFilter() {
+		FilterRegistrationBean<CorsFilter> registrationBean = new FilterRegistrationBean<>();
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+
+		// Allow every origin (just for testing)
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+
+		// Apply configuration to all paths
+		source.registerCorsConfiguration("/**", config);
+		registrationBean.setFilter(new CorsFilter(source));
+
+		// First filter in filter chain
+		registrationBean.setOrder(0);
 
 		return registrationBean;
 	}
